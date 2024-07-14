946eabe2317d23198f54ddc151c142d54c1222d8
沉鱼 <1162151166@qq.com>
2024-07-14 16:38:54 +0800

    Delete NIM_Web_Demo_ReactNative directory

--- NIM_Web_Demo_ReactNative/src/res/index.js
+++
@@ -1,6 +0,0 @@
-export default {
-  loadingMsg: require('./loading-msg.gif'),
-  loadingImg: require('./loading-img.gif'),
-  page404: require('./404.png'),
-};
-
--- NIM_Web_Demo_ReactNative/src/store/constant.js
+++
@@ -1,8 +0,0 @@
-const constant = {
-  nim: null,
-  chatroom: null,
-  // 全局只有一个声音
-  sound: null,
-};
-
-export default constant;
